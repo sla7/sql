e8b85d7f26a530348cc0f4c08f9e48badee65b5d
sla7 <33646780+sla7@users.noreply.github.com>
2017-11-27 02:12:32 -0800

    Update sqlproject.sql

--- sqlproject.sql
+++ sqlproject.sql
@@ -4,6 +4,8 @@ create table projectdraft.customer
   Last_Name VARCHAR(15),
   primary key (Customer_id));
 
+insert into projectdraft.customer VALUES ('Steven','La')
+
 
 create table projectdraft.billinginfo
   (Billinginfo_id INT IDENTITY(100,1) NOT NULL,
@@ -15,6 +17,9 @@ create table projectdraft.billinginfo
   Customer_id   INT,
   PRIMARY KEY (billinginfo_id),
   foreign key (Customer_id) references projectdraft.customer);
+
+insert into projectdraft.billinginfo values ('Steven', '1560 shumaker','sanjose','ca','95131', 1);
+
 
 create TABLE projectdraft.shipping_address_info
   (Address_id INT IDENTITY(200,1) NOT NULL,
@@ -28,6 +33,8 @@ create TABLE projectdraft.shipping_address_info
   PRIMARY KEY (Address_id),
   FOREIGN KEY (Customer_id) references projectdraft.customer);
 
+insert into projectdraft.shipping_address_info values (1, '3975 freedom cicle','n/a','Santa Clara', 'CA', '95131', '408-503-0289')
+
 create table projectdraft.orders
   (Orders_id INT IDENTITY(300,1) NOT NULL,
   Order_date DATETIME,
@@ -38,12 +45,19 @@ create table projectdraft.orders
   PRIMARY KEY (orders_id),
   FOREIGN KEY (Customer_id) REFERENCES projectdraft.customer);
 
+insert into projectdraft.orders VALUES (getdate(), 'paid', '1666 Foxy', '99.99', 1)
+
+
+
 create table projectdraft.payment_type
   (
   Payment_type_id INT IDENTITY(400,1) NOT NULL,
   Payment_type    VARCHAR(15),
   PRIMARY KEY (Payment_type_id)
   );
+
+insert into projectdraft.payment_type values ('VISA')
+
 
 create table projectdraft.paymentmethod
   (Payment_id INT IDENTITY(500,1) NOT NULL,
@@ -57,6 +71,9 @@ create table projectdraft.paymentmethod
   FOREIGN KEY (Customer_id) REFERENCES projectdraft.customer,
   FOREIGN KEY (Payment_type_id) REFERENCES projectdraft.payment_type);
 
+insert into projectdraft.paymentmethod VALUES (1, 400 ,'4352-5423-6675-4404','Steven La','2200/1/20','234')
+
+
 create table projectdraft.orderpayment
   (Order_id INT,
   Payment_id INT,
@@ -64,11 +81,33 @@ create table projectdraft.orderpayment
   FOREIGN KEY (Order_id) REFERENCES projectdraft.orders,
   FOREIGN KEY (Payment_id) REFERENCES projectdraft.paymentmethod);
 
+insert into projectdraft.orderpayment VALUES (300, 500, 'PAID')
+
 create table projectdraft.product_type
   (Prodtype_id INT IDENTITY(700,1) NOT NULL,
   Prodtype_name VARCHAR(15)
   PRIMARY KEY (Prodtype_id)
   );
+
+insert into projectdraft.product_type VALUES ('Digital')
+insert into projectdraft.product_type VALUES ('Merch')
+insert into projectdraft.product_type VALUES ('Tickets')
+insert into projectdraft.product_type VALUES ('Album')
+
+create table projectdraft.inventory
+  (Inventory_id INT IDENTITY(1300,1) NOT NULL,
+  Sku_id VARCHAR(15) UNIQUE ,
+  Product_id VARCHAR(15),
+  Prodtype_id INT,
+  Returnable VARCHAR(15),
+  PRIMARY KEY (Product_id),
+  FOREIGN KEY (Prodtype_id) REFERENCES projectdraft.product_type
+  )
+
+INSERT into projectdraft.inventory VALUES (concat(1300, 'A1'),'A1',703,'No')
+INSERT into projectdraft.inventory VALUES (concat(1300, 'T1'),'T1',700,'No')
+INSERT into projectdraft.inventory VALUES (concat(1300, 'M1'),'M1',701,'No')
+INSERT into projectdraft.inventory VALUES (concat(1300, 'TICK1'),'TICK1',702,'No')
 
 create table projectdraft.artist
   (Artist_id INT IDENTITY(800,1) NOT NULL,
@@ -76,37 +115,48 @@ create table projectdraft.artist
   PRIMARY KEY (Artist_id)
   )
 
+INSERT into projectdraft.artist VALUES ('Drake')
+
 create TABLE projectdraft.album
-  (Album_id INT IDENTITY(900,1) NOT NULL,
+  (Album_id VARCHAR(15),
   Album_name VARCHAR(15),
   Album_realease_year VARCHAR(15),
   Prodtype_id INT,
   Artist_id INT,
   Album_price VARCHAR(15),
   PRIMARY KEY (Album_id),
+  FOREIGN KEY (Album_id) REFERENCES projectdraft.inventory(Product_id),
   FOREIGN KEY (Artist_id) REFERENCES projectdraft.artist,
   FOREIGN KEY (Prodtype_id) REFERENCES projectdraft.product_type);
-  
+
+insert into projectdraft.album VALUES ('A1','Thank Me Later', 2010, 703, 800, 9.99)
+
 create table projectdraft.track
-  (Track_id INT IDENTITY(1000,1) NOT NULL,
+  (Track_id VARCHAR(15),
   Track_name VARCHAR(15),
   Album_id VARCHAR(15),
   Artist_id INT,
   Prodtype_id VARCHAR(15),
   Track_price VARCHAR(15),
   PRIMARY KEY (Track_id),
+  FOREIGN KEY (Track_id) REFERENCES projectdraft.inventory(Product_id),
   FOREIGN KEY (Artist_id) REFERENCES projectdraft.artist);
 
+insert into projectdraft.track VALUES ('T1','Fireworks',900, 800, 700, 0.99)
+
 create table projectdraft.merchandise
-  (Merch_id INT IDENTITY(1100,1) NOT NULL,
+  (Merch_id VARCHAR(15),
   Merchandise_name VARCHAR(15),
   Prodtype_id VARCHAR(15),
   Artist_id INT,
   PRIMARY KEY (Merch_id),
+  FOREIGN KEY (Merch_id) REFERENCES projectdraft.inventory(Product_id),
   FOREIGN KEY (Artist_id) REFERENCES projectdraft.artist);
 
+insert into projectdraft.merchandise VALUES ('M1','Drake T-SHIRT', 701, 800 )
+
 create table projectdraft.ticket
-  (Ticket_id INT IDENTITY(1200,1) NOT NULL,
+  (Ticket_id VARCHAR(15),
   Event_name VARCHAR(15),
   Event_date Date,
   Event_Location VARCHAR(15),
@@ -114,53 +164,26 @@ create table projectdraft.ticket
   Prodtype_id INT,
   Artist_id INT,
   PRIMARY KEY (Ticket_id),
+  FOREIGN KEY (Ticket_id) REFERENCES projectdraft.inventory(Product_id),
   FOREIGN KEY (Artist_id) REFERENCES projectdraft.artist,
   FOREIGN KEY (Prodtype_id) REFERENCES projectdraft.product_type
   )
 
-create table projectdraft.inventory
-  (Inventory_id INT IDENTITY(1300,1) NOT NULL,
-  Sku_id VARCHAR(15),
-  Category INT,
-  Product_id INT,
-  Prodtype_id INT,
-  Returnable VARCHAR(15),
-  PRIMARY KEY (Inventory_id),
-  FOREIGN KEY (Product_id) REFERENCES projectdraft.artist,
-  FOREIGN KEY (Product_id) REFERENCES projectdraft.track,
-  FOREIGN KEY (Product_id) REFERENCES projectdraft.merchandise,
-  FOREIGN KEY (Product_id) REFERENCES projectdraft.ticket,
-  FOREIGN KEY (Prodtype_id) REFERENCES projectdraft.product_type,
-  FOREIGN KEY (Prodtype_id) REFERENCES projectdraft.album
-  )
+INSERT into projectdraft.ticket VALUES ('TICK1','Drake Concert', '2018/01/20', 'SAP Center', 59.99, 702, 800)
 
 create table projectdraft.order_line
 (
   Order_Line_id   INT IDENTITY(1400,1) NOT NULL,
   Order_id        INT,
   Quantity        VARCHAR(15),
-  Unit_price      VARCHAR(15),
+  total_price      VARCHAR(15),
   Sku_id          VARCHAR(15),
-  Item_name       VARCHAR(15),
   Purchase_date   VARCHAR(15),
   Returnable_date VARCHAR(15),
   PRIMARY KEY (Order_Line_id),
-  FOREIGN KEY (Order_id) REFERENCES projectdraft.orders
+  FOREIGN KEY (Order_id) REFERENCES projectdraft.orders,
+  FOREIGN KEY (Sku_id) REFERENCES projectdraft.inventory(Sku_id)
 )
 
-insert into projectdraft.customer VALUES ('Steven','La');
-insert into projectdraft.billinginfo values ('Steven', '1560 shumaker','sanjose','ca','95131', 1);
-insert into projectdraft.shipping_address_info values (1, '3975 freedom cicle','n/a','Santa Clara', 'CA', '95131', '408-503-0289');
-insert into projectdraft.orders VALUES (getdate(), 'paid', '1666 Foxy', '99.99', 1);
-insert into projectdraft.payment_type values ('VISA');
-insert into projectdraft.paymentmethod VALUES (1, 400 ,'4352-5423-6675-4404','Steven La','2200/1/20','234');
-insert into projectdraft.orderpayment VALUES (300, 500, 'PAID');
-insert into projectdraft.product_type VALUES ('Digital');
-insert into projectdraft.product_type VALUES ('Merch');
-insert into projectdraft.product_type VALUES ('Tickets');
-insert into projectdraft.product_type VALUES ('Album');
-INSERT into projectdraft.artist VALUES ('Drake');
-insert into projectdraft.album VALUES ('Thank Me Later', 2010, 700, 800, 9.99);
-insert into projectdraft.track VALUES ('Fireworks',900, 800, 700, 0.99);
-insert into projectdraft.merchandise VALUES ('Drake T-SHIRT', 701, 800 );
-INSERT into projectdraft.ticket VALUES ('Drake Concert', '2018/01/20', 'SAP Center', 59.99, 702, 800);
+INSERT INTO projectdraft.order_line VALUES (300, 5, 100, '1300A1', GETDATE(), dateadd(day, 30, getdate()))
+
