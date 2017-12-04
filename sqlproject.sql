12cfb048e5ad062f26eb6c285d539eb36c75bdaf
emvh <34050918+emvh@users.noreply.github.com>
2017-12-03 16:19:14 -0800

    Update sqlproject.sql

--- sqlproject.sql
+++ sqlproject.sql
@@ -67,21 +67,21 @@ INSERT INTO project.shipping_info VALUES ('Cat','Smith','200 Apple Way',' ','San
 /* ORDERS */
 
 CREATE TABLE project.orders(
-  Orders_id       int IDENTITY(300,1) NOT NULL UNIQUE,
-  Order_number    int,
-  Order_date      DATE,
-  Payment_status  VARCHAR(15),
-  Customer_id     int,
-  Total           float,
-  PRIMARY KEY (orders_id),
-  FOREIGN KEY (Customer_id) REFERENCES project.customer);
+  Order_id int IDENTITY(300,1) NOT NULL,
+  Order_number int,
+  Order_date date,
+  Payment_status varchar(15),
+  Customer_id int,
+  Total float,
+  PRIMARY KEY (order_id),
+  FOREIGN KEY (Customer_id) REFERENCES project.customer
+);
 
-INSERT INTO project.orders VALUES (1,getdate(),'Paid',1, 45.00)
-INSERT INTO project.orders VALUES (2,getdate(),'Paid',1, 11.99)
-INSERT INTO project.orders VALUES (3,getdate(),'Paid',1, 65.00)
-INSERT INTO project.orders VALUES (4,getdate(),'Paid',2, 1.29)
-INSERT INTO project.orders VALUES (5,getdate(),'Paid',3, 20.00)
-INSERT INTO project.orders VALUES (6,getdate(),'Paid',4, 25.00)
+INSERT INTO project.orders VALUES (1,'12/04/2017','Paid',1,150.30);
+INSERT INTO project.orders VALUES (2,'11/01/2017','Paid',1,51.99);
+INSERT INTO project.orders VALUES (3,'09/10/2017','Paid',1,6.99);
+INSERT INTO project.orders VALUES (4,'10/22/2017','Paid',2,0.99);
+INSERT INTO project.orders VALUES (5,'11/15/2017','Refunded',3,-25.00);
 
 
 
@@ -89,9 +89,9 @@ INSERT INTO project.orders VALUES (6,getdate(),'Paid',4, 25.00)
 
 CREATE TABLE project.payment_type(
   Payment_type_id int IDENTITY(400,1) NOT NULL,
-  Payment_type    varchar(30),
-  PRIMARY KEY     (Payment_type_id)
-  );
+  Payment_type varchar(30),
+  PRIMARY KEY (Payment_type_id)
+);
 
 INSERT INTO project.payment_type values ('American Express');
 INSERT INTO project.payment_type values ('Discover');
@@ -101,14 +101,14 @@ INSERT INTO project.payment_type values ('Visa');
 /* PAYMENT METHOD */
 
 CREATE TABLE project.paymentmethod(
-  Payment_id      int IDENTITY(500,1) NOT NULL,
-  Customer_id     int,
+  Payment_id int IDENTITY(500,1) NOT NULL,
+  Customer_id int,
   Payment_type_id int,
-  Card_Number     VARCHAR(19),
-  NameOnCard      VARCHAR(50),
-  CardExp         DATE,
-  Card_CSV_code   VARCHAR(15),
-  Billing_id      int,
+  Card_Number varchar(19),
+  NameOnCard varchar(50),
+  CardExp date,
+  Card_CSV_code varchar(15),
+  Billing_id int,
   PRIMARY KEY (Payment_id),
   FOREIGN KEY (Customer_id) REFERENCES project.customer(Customer_id),
   FOREIGN KEY (Payment_type_id) REFERENCES project.payment_type,
@@ -117,7 +117,7 @@ CREATE TABLE project.paymentmethod(
 
 INSERT INTO project.paymentmethod VALUES (1,400,'4352-5423-6675-4404','Steven La','01/20/2022','234',100);
 INSERT INTO project.paymentmethod VALUES (2,400,'4352-5423-6675-3333','Ken Ohata','02/18/2019','200',101);
-INSERT INTO project.paymentmethod VALUES (3,400,'4352-5423-6675-1111','Andrew Lee','03/05/2020','196',102);
+INSERT INTO project.paymentmethod VALUES (3,400,'4352-5423-6675-9305','Andrew Lee','08/05/2018','196',102);
 INSERT INTO project.paymentmethod VALUES (4,400,'4352-5423-6675-2222','Emily Ho','04/10/2021','789',103);
 INSERT INTO project.paymentmethod VALUES (5,400,'4352-5423-6675-4000','Alice James','05/07/2022','453',104);
 INSERT INTO project.paymentmethod VALUES (6,400,'4352-5423-6675-5000','Will Smith','06/11/2019','567',105);
@@ -130,7 +130,8 @@ create table project.orderpayment(
   Payment_id int,
   Payment_status varchar(15)
   FOREIGN KEY (Order_id) REFERENCES project.orders,
-  FOREIGN KEY (Payment_id) REFERENCES project.paymentmethod);
+  FOREIGN KEY (Payment_id) REFERENCES project.paymentmethod
+);
 
 INSERT INTO project.orderpayment VALUES (300, 500, 'PAID');
 INSERT INTO project.orderpayment VALUES (301, 501, 'PAID');
@@ -186,38 +187,42 @@ INSERT INTO project.artist VALUES ('Cardi B');
 INSERT INTO project.artist VALUES ('Luke Bryan');
 INSERT INTO project.artist VALUES ('Demi Lovato');
 INSERT INTO project.artist VALUES ('98 Degrees');
-INSERT INTO project.artist VALUES ('Thomas Dausgaard');
+INSERT INTO project.artist VALUES ('Seattle Symphony Orchestra');
+INSERT INTO project.artist VALUES ('David Bowie');
+INSERT INTO project.artist VALUES ('Frank Ocean');
 
 
 CREATE TABLE project.track(
   Track_id varchar(15),
   Track_name varchar(50),
   Genre varchar(50),
+  Label varchar(100),
   Album_id int,
   Artist_id int,
   Prodtype_id varchar(15),
   Track_price float,
   PRIMARY KEY (Track_id),
-  FOREIGN KEY (Artist_id) REFERENCES project.artist(Artist_id)
+  FOREIGN KEY (Artist_id) REFERENCES projectdraft.artist(Artist_id)
 );
 
-INSERT INTO project.track VALUES ('T1','Lose Yourself','Hip Hop',900, 800, 700, 0.79);
-INSERT INTO project.track VALUES ('T2','Wolves','Hip Hop',901, 801, 700, 0.99);
-INSERT INTO project.track VALUES ('T3','Numb','Rock',902, 802, 700, 0.99);
-INSERT INTO project.track VALUES ('T4','Too Good at Goodbyes','Pop',903, 803, 700, 0.99);
-INSERT INTO project.track VALUES ('T5','Hello','Pop',904, 804, 700, 1.29);
-INSERT INTO project.track VALUES ('T6','Fireworks','R&B',905, 805, 700, 1.29);
-INSERT INTO project.track VALUES ('T7','In The Morning','Hip Hop',906, 806, 700, 1.29);
-INSERT INTO project.track VALUES ('T8','Havana','Pop',907, 808, 700, 1.29);
-INSERT INTO project.track VALUES ('T9','Shape of You','Pop',NULL, 809, 700, 1.29);
+INSERT INTO project.track VALUES ('T1','Lose Yourself','Hip Hop','Interscope',900, 800, 700, 0.79);
+INSERT INTO project.track VALUES ('T2','Wolves','Hip Hop','Rock the World',901, 801, 700, 0.99);
+INSERT INTO project.track VALUES ('T3','Numb','Rock','Warner Bros',902, 802, 700, 0.99);
+INSERT INTO project.track VALUES ('T4','Too Good at Goodbyes','Capitol','Pop',903, 803, 700, 0.99);
+INSERT INTO project.track VALUES ('T5','Hello','Pop','XI Recordings',904, 804, 700, 1.29);
+INSERT INTO project.track VALUES ('T6','Fireworks','R&B','Young Money Entertainment',905, 805, 700, 1.29);
+INSERT INTO project.track VALUES ('T7','In The Morning','Hip Hop','Columbia',906, 806, 700, 1.29);
+INSERT INTO project.track VALUES ('T8','Havana','Pop','Epic Records',907, 808, 700, 1.29);
+INSERT INTO project.track VALUES ('T9','Shape of You','Pop','Asylum Records',NULL, 809, 700, 1.29);
 
 
 
 CREATE TABLE project.album(
   Album_id varchar(15),
-  Album_name varchar(100),
+  Album_name varchar(200),
   Album_realease_year int,
   Genre varchar(50),
+  Label varchar(100),
   Prodtype_id int,
   Artist_id int,
   Album_price float,
@@ -226,22 +231,25 @@ CREATE TABLE project.album(
   FOREIGN KEY (Prodtype_id) REFERENCES project.product_type(Prodtype_id)
 );
 
-INSERT INTO project.album VALUES ('A1','8 Mile', 2002, 'Hip Hop', 701, 800, 6.99);
-INSERT INTO project.album VALUES ('A2','The Life of Pablo', 2004, 'Hip Hop', 701, 801, 11.99);
-INSERT INTO project.album VALUES ('A3','Meteora', 2003, 'Rock', 701, 802, 7.99);
-INSERT INTO project.album VALUES ('A4','The Thrill of It All', 2017, 'Pop', 701, 803, 9.99);
-INSERT INTO project.album VALUES ('A5','25', 2015, 'Pop', 701, 804, 10.99);
-INSERT INTO project.album VALUES ('A6','Thank Me Later', 2010, 'R&B', 701, 805, 6.99);
-INSERT INTO project.album VALUES ('A7','Cole World', 2011, 'Hip Hop', 701, 806, 11.99);
-INSERT INTO project.album VALUES ('A8','8 Mile', 2002, 'Hip Hop', 702, 800, 6.99);
-INSERT INTO project.album VALUES ('A9','The Life of Pablo', 2004, 'Hip Hop', 702, 801, 11.99);
-INSERT INTO project.album VALUES ('A10','Meteora', 2003, 'Rock', 702, 802, 7.99);
-INSERT INTO project.album VALUES ('A11','The Thrill of It All', 2017, 'Pop', 702, 803, 9.99);
-INSERT INTO project.album VALUES ('A12','25', 2015, 'Pop', 702, 804, 10.99);
-INSERT INTO project.album VALUES ('A13','Thank Me Later', 2010, 'R&B', 702, 805, 6.99);
-INSERT INTO project.album VALUES ('A14','Cole World', 2011, 'Hip Hop', 702, 806, 11.99);
-INSERT INTO project.album VALUES ('A15','Heartbreak On A Full Moon', 2017, 'Hip Hop', 701, 807, 16.99);
-INSERT INTO project.album VALUES ('A16','What Makes You Country', 2017, 'Country', 701, 818, 9.99);
+INSERT INTO project.album VALUES ('A1','8 Mile', 2002, 'Hip Hop','Interscope', 701, 800, 6.99);
+INSERT INTO project.album VALUES ('A2','The Life of Pablo', 2004, 'Hip Hop','Rock the World', 701, 801, 11.99);
+INSERT INTO project.album VALUES ('A3','Meteora', 2003, 'Rock','Warner Bros', 701, 802, 7.99);
+INSERT INTO project.album VALUES ('A4','The Thrill of It All', 2017, 'Pop','Capitol',701, 803, 9.99);
+INSERT INTO project.album VALUES ('A5','25', 2015, 'Pop', 'XI Recordings',701, 804, 10.99);
+INSERT INTO project.album VALUES ('A6','Thank Me Later', 2010, 'R&B','Young Money Entertainment', 701, 805, 6.99);
+INSERT INTO project.album VALUES ('A7','Cole World', 2011, 'Hip Hop','Columbia', 701, 806, 11.99);
+INSERT INTO project.album VALUES ('A8','8 Mile', 2002, 'Hip Hop', 'Interscope',702, 800, 6.99);
+INSERT INTO project.album VALUES ('A9','The Life of Pablo', 2004, 'Hip Hop','Rock the World', 702, 801, 11.99);
+INSERT INTO project.album VALUES ('A10','Meteora', 2003, 'Rock', 'Warner Bros',702, 802, 7.99);
+INSERT INTO project.album VALUES ('A11','The Thrill of It All', 2017, 'Pop','Capitol', 702, 803, 9.99);
+INSERT INTO project.album VALUES ('A12','25', 2015, 'Pop','XI Recordings', 702, 804, 10.99);
+INSERT INTO project.album VALUES ('A13','Thank Me Later', 2010, 'R&B','Young Money Entertainment', 702, 805, 6.99);
+INSERT INTO project.album VALUES ('A14','Cole World', 2011, 'Hip Hop','Columbia',702, 806, 11.99);
+INSERT INTO project.album VALUES ('A15','Heartbreak On A Full Moon', 2017, 'Hip Hop','RCA Records', 701, 807, 16.99);
+INSERT INTO project.album VALUES ('A16','What Makes You Country', 2017, 'Country','Capitol Nashville', 701, 818, 9.99);
+INSERT INTO project.album VALUES ('A17','Nielson: Symphonies Nos. 3 & 4', 2017, 'Classical','Seattle Symphony Media',701, 822, 6.99);
+INSERT INTO project.album VALUES ('A18','Atomic Blonde', 2017, 'Rock','Back Lot Music', 701, 823, 9.99);
+INSERT INTO project.album VALUES ('A19','Blonde', 2017, 'R&B','Boys Dont Cry', 701, 824, 9.99);
 
 
 
@@ -279,7 +287,7 @@ CREATE TABLE project.ticket(
   PRIMARY KEY (Ticket_id),
   FOREIGN KEY (Artist_id) REFERENCES project.artist(Artist_id),
   FOREIGN KEY (Prodtype_id) REFERENCES project.product_type(Prodtype_id)
-  );
+ );
 
 INSERT INTO project.ticket VALUES ('TICK1','Eminem Concert', '08/20/2018', 'SAP Center',800,704,65.00);
 INSERT INTO project.ticket VALUES ('TICK2','Kanye Concert', '09/25/2018', 'Oracle Coliseum',801,704,40.00);
@@ -306,7 +314,7 @@ CREATE TABLE project.inventory(
   Returnable varchar(15),
   PRIMARY KEY (Inventory_id),
   FOREIGN KEY (Prodtype_id) REFERENCES project.product_type(Prodtype_id)
-  );
+ );
 
 INSERT INTO project.inventory(Product_id, SKU_id,Prodtype_id,Quantity,returnable)
 SELECT project.track.track_id,concat('X1',Track_id),Prodtype_id,1000,'No'
@@ -329,52 +337,6 @@ FROM project.merchandise;
 INSERT INTO project.inventory(Product_id, SKU_id,Prodtype_id,Quantity,returnable)
 SELECT project.ticket.ticket_id,concat('X1',Ticket_id),Prodtype_id,1000,'No'
 FROM project.ticket;
-
-
-
-
-/* ORDER LINE - STILL NEED TO ADD DATA FOR THIS -- WILL TRY LATER TONIGHT */
-
-create table project.order_line
-(
-  Order_Line_id   INT IDENTITY(1400,1) NOT NULL,
-  Order_id        INT,
-  Quantity        int,
-  Sku_id          VARCHAR(15),
-  Purchase_date   VARCHAR(15),
-  Returnable_date VARCHAR(15),
-  PRIMARY KEY (Order_Line_id),
-  FOREIGN KEY (Order_id) REFERENCES project.orders,
-  FOREIGN KEY (Sku_id) REFERENCES project.inventory(Sku_id)
-)
-
-INSERT INTO project.order_line VALUES (300, 5,'X1T1', GETDATE(), dateadd(day, 30, getdate()))
-
-
-
-/* ORDER LINE */
-
-CREATE TABLE projectdraft.order_line(
-  Order_Line_id int IDENTITY(1400,1) NOT NULL,
-  Order_id int,
-  Order_type varchat(15),
-  Quantity int,
-  Sku_id varchar(15),
-  Purchase_date date,
-  Returnable_date date,
-  Original_Order_id int,
-  PRIMARY KEY (Order_Line_id),
-  FOREIGN KEY (Order_id) REFERENCES projectdraft.orders(Order_id),
-  FOREIGN KEY (SKU_id) REFERENCES projectdraft.inventory(SKU_id)
-);
-
-INSERT INTO projectdraft.order_line VALUES (300,'Purchase', 2,'X1TICK12', 12/04/2017, NULL, NULL);
-INSERT INTO projectdraft.order_line VALUES (301,'Purchase', 1,'X1M6', 11/01/2017, dateadd(day, 30, getdate()), NULL);
-INSERT INTO projectdraft.order_line VALUES (301,'Purchase', 1,'X1M2', 11/01/2017, dateadd(day, 30, getdate()), NULL);
-INSERT INTO projectdraft.order_line VALUES (301,'Purchase', 2,'X1A13', 11/01/2017, dateadd(day, 30, getdate()), NULL);
-INSERT INTO projectdraft.order_line VALUES (302,'Purchase', 1,'X1A17', 09/10/2017, NULL, NULL);
-INSERT INTO projectdraft.order_line VALUES (303,'Purchase', 1,'X1T4', 10/22/2017, NULL, NULL);
-INSERT INTO projectdraft.order_line VALUES (304,'Return', 1,'X1M6', 11/15/2017, NULL, 301);
 
 
 
